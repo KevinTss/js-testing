6c4d0b8692377dc25a60f6a7dee8a2877cf84db0
Kevin <kevin.tassi@gmail.com>
2019-12-17 21:23:35 +0100

    Add example with expect

--- test/calcul.spec.js
+++ test/calcul.spec.js
@@ -1,9 +1,16 @@
 const assert = require("chai").assert;
+const expect = require("chai").expect;
 
 const Calcul = require("../lib/Calcul");
 
 describe("Calcul", () => {
+  it("Should be a function", () => {
+    expect(Calcul.add).to.be.a("function");
+  });
   it("Should add numbers", () => {
     assert.equal(Calcul.add(2, 2, 3), 7);
+  });
+  it("Should add numbers wrong", () => {
+    expect(Calcul.add(2, 2, 3)).to.not.equal(6);
   });
 });

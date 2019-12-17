56fd5988b767b65e8348339a2c35ea397733184d
Kevin Tassi <kevin@wooclap.com>
2019-12-17 18:05:47 +0100

    fix typo

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 
 describe("Examples tests", () => {
-  it("Shoule double a number", () => {
+  it("Should double a number", () => {
     const double = nb => nb * 2;
     const nb = 2;
     assert.equal(double(nb), 4);
